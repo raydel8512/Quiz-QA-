8aec0c1726375f369042f7a24c442778391907c8
raydel8512 <69324125+raydel8512@users.noreply.github.com>
2022-07-14 18:58:46 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quien será la nueva supervisora?",
+    answer: "Gabriela",
+    options: [
+      "Gabriela",
+      "Alessa",
+      "Raydel",
+      "Dotto"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Debe salir Dotto de almuerzo?",
+    answer: "No",
+    options: [
+      "No",
+      "Mas tarde",
+      "Despues de las 17h",
+      "Mañana"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quien va a hacer la WBR la semana próxima?",
+    answer: "Rosa",
+    options: [
+      "Dotto",
+      "Gabriela",
+      "Raydel",
+      "Alessa"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que es Raydel?",
+    answer: "Un moustro",
+    options: [
+      "Un bobo",
+      "Un loco",
+      "Hermoso",
+      "Un moustro"
+    ]
+  },
+    {
+    numb: 5,
+    question: "De los 4 quien esta siempre podre?",
+    answer: "Dotto",
+    options: [
+      "Raydel",
+      "Gabriela",
+      "Dotto",
+      "Alessa"
+    ]
+  },
+  // puedes descomentar los códigos a continuación y duplicarlos tanto como desee agregar una pregunta
+  // pero recuerda que necesitas dar el valor adormecido serializar como 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
